951b5ddc0cd636f800eeea91af264e525282b37e
Zayne Anthony <zayne@rocketcitydigital.com>
2022-01-15 08:15:10 -0600

    FAQ Update

--- components/FAQ.js
+++ components/FAQ.js
@@ -9,9 +9,12 @@ export default function FAQ() {
 						Lemonoodles are a collection of unique, programmatically generated
 						frens inspired by Little Lemon Friends and Doodles. We like the
 						lemons and we like the doodles. Turns out they liked each other too.
-						These are their love children and a story of forbidden love. Note,
-						the art for Lemonoodles is completely our own and is only inspired
-						by and not affiliated with Doodles or Little Lemon Friends.
+						These are their love children and a story of forbidden love.
+					</p>
+					<p>
+						Note, the art for Lemonoodles is completely our own and each trait
+						has been hand drawn. The project is only inspired by and not
+						affiliated with Doodles or Little Lemon Friends.
 					</p>
 				</Accordian>
 				<Accordian title='How many Lemonoodles are there?'>
@@ -34,15 +37,15 @@ export default function FAQ() {
 					<p>
 						Yes! To reward the communities of our inspiration projects and save
 						everyone gas we will be opening the pre-sale to holders of Doodles
-						and Little Lemon friends.
+						and Little Lemon friends. We have also opened it up to members of
+						our community and will be rewarding other spots based on activity
+						and overall encouragement of others!!
 					</p>
 				</Accordian>
 				<Accordian title='Wen Mint?'>
 					<p>
 						Official time will be announced soon! Our Pre-sale will start first
-						with the public sale opening within 24 hours. Could be 1 hour after
-						or could be 23 hours after. Stick around the Discord to find out
-						because that's where we're announcing it!
+						with the public sale opening 24 hours after.
 					</p>
 				</Accordian>
 				<Accordian title='Will there be a reveal?'>
@@ -51,40 +54,51 @@ export default function FAQ() {
 					</p>
 				</Accordian>
 				<Accordian title="What's the mint limit per transaction/wallet for Pre-sale?">
-					<p>Pre-sale: 2 Lemonoodles per wallet.</p>
+					<p>
+						Pre-sale: 2 Lemonoodles per collection per wallet and 2 for those
+						with WL roles!
+					</p>
 				</Accordian>
 				<Accordian title="What's the mint limit per transaction/wallet for Public sale?">
 					<p>
-						Public sale: Max of 20 Lemonoodles per wallet. All remaining supply
-						after Pre-sale will be available to mint during the Public sale.
+						Public sale: All remaining supply after Pre-sale will be available
+						to mint during the Public sale. Max of 10 Lemonoodles per
+						transaction.
 					</p>
 				</Accordian>
 				<Accordian title="How do I know I'm on the Whitelist?">
 					<p>
 						If you are a holder of a Doodle or Little Lemon Friend then you will
-						be able to freely mint when the pre-sale opens!
+						be able to freely mint when the pre-sale opens! If you have the
+						LEMONLIST role in the Discord you also are WL'd. Please please
+						please make sure to submit your wallet address in the WL channel!
 					</p>
 				</Accordian>
 				<Accordian title='Is there a launch roadmap?'>
 					<p>
 						We're all about being chill here. Unlike other projects we're not
-						really into promising the world and delivering nothing! This is a
-						community driven project at its core and major decisions will be
-						made by YOU the community! Once the project is sold out we may have
-						a few surprises for those that hang around the Discord! After
-						community feedback on further direction we will be hosting a series
-						of votes in the verified Lemonoodles polls channel.
+						really into creating artificial hype, promising the world, and then
+						delivering nothing! We have a capable team that can build almost
+						anything imaginable! We want to leave that up to you as a community
+						to decide though! See the Lemon-Map channel for more updates!
+					</p>
+					<p>
+						Once the project is sold out we MAY have a few surprises and
+						giveaways 😉 for those that hang around the Discord!
 					</p>
 				</Accordian>
 				<Accordian title='What commercial rights do I have to my Lemonoodles?'>
 					<p>
 						You are free to do whatever you want with your Lemonoodle!! The IP
-						is yours!! Lemonoodles are in the Public Domain. To the extent
-						possible under law, we have waived all copyright and related or
-						neighboring rights to Lemonoodles. We believe that by releasing the
-						works under CC0 we allow the community to freely build upon, enhance
-						and reuse the works for any purposes without fear or restriction
-						under copyright and database law
+						is yours!!
+					</p>
+					<p>
+						Lemonoodles are in the Public Domain. To the extent possible under
+						law, we have waived all copyright and related or neighboring rights
+						to Lemonoodles. We believe that by releasing the works under CC0 we
+						allow the community to freely build upon, enhance and reuse the
+						works for any purposes without fear or restriction under copyright
+						and database law
 					</p>
 				</Accordian>
 			</div>
