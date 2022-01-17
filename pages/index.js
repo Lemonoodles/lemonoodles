279be1a81ad4544c070001049c99ac1f971d634d
zayneanthonyrcd <zayne@rocketcitydigital.com>
2022-01-17 14:46:13 -0600

    Update

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,9 @@ export default function Home() {
 							<div className='max-w-max mx-auto'>
 								<Image src={logo} alt='Lemonoodles NFT' />
 							</div>
-							{/* <p className='outline-text font-skrap mx-auto text-6xl text-center'>
-								7,777 NFTs / 0.035 ETH
-							</p> */}
+							<p className='outline-text font-skrap mx-auto !mb-6 text-6xl text-center'>
+								0.035 ETH Each
+							</p>
 							<Mint />
 						</div>
 					</div>
